cbf546488b53a6fc9435598d31e385250876c9f0
Christopher M <christopher_mao@branson.org>
2024-09-21 15:43:42 -0700

    add DriveIO

---
+++ src/main/java/frc/robot/subsystems/drive/DriveIO.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.drive;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface DriveIO {
+    @AutoLog
+    public static class DriveIOInputs {
+        public double motorAppliedVolts = 0.0;
+        public double motorCurrentAmps = 0.0;
+
+        public double leftSpeed = 0.0;
+        public double rightSpeed = 0.0;
+    }
+
+    default void updateInputs(DriveIOInputs inputs) {}
+
+    // stop, run
+    default void motorStop() {}
+
+    default void motorRun(double inputVolts){} 
+} 
