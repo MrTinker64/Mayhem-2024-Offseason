38429fe7adadd173a175535dfa3c26ea4844498a
Christopher M <christopher_mao@branson.org>
2024-09-21 15:43:58 -0700

    Create DriveConstants

---
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.drive;
+
+public class DriveConstants {
+    // TODO: Change these ids
+    public static final int rightMotorID = 0;
+    public static final int leftMotorID = 0;
+}
