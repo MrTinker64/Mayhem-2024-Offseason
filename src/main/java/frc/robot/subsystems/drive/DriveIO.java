03d323991e93fd2a43b81b700999d38fdf906a0f
grassEqualsBugs <eizprintz@gmail.com>
2024-10-19 14:13:38 -0700

    Seperate left and right applied volts and current amps

--- src/main/java/frc/robot/subsystems/drive/DriveIO.java
+++ src/main/java/frc/robot/subsystems/drive/DriveIO.java
@@ -5,11 +5,13 @@ import org.littletonrobotics.junction.AutoLog;
 public interface DriveIO {
   @AutoLog
   public static class DriveIOInputs {
-    public double motorAppliedVolts = 0.0;
-    public double motorCurrentAmps = 0.0;
-
     public double leftPosition = 0.0;
     public double rightPosition = 0.0;
+    
+    public double leftAppliedVolts = 0.0;
+    public double rightAppliedVolts = 0.0;
+    public double leftCurrentAmps = 0.0;
+    public double rightCurrentAmps = 0.0;
   }
 
   default void updateInputs(DriveIOInputs inputs) {}
