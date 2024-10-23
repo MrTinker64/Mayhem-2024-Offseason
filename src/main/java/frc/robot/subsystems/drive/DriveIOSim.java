619a693242485572aff9224f720b1d83d5acd983
grassEqualsBugs <eizprintz@gmail.com>
2024-10-23 08:44:25 -0700

    Make robot faster

--- src/main/java/frc/robot/subsystems/drive/DriveIOSim.java
+++ src/main/java/frc/robot/subsystems/drive/DriveIOSim.java
@@ -23,7 +23,7 @@ import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotWheelSiz
 public class DriveIOSim implements DriveIO {
   private DifferentialDrivetrainSim sim =
       DifferentialDrivetrainSim.createKitbotSim(
-          KitbotMotor.kDualCIMPerSide, KitbotGearing.k10p71, KitbotWheelSize.kSixInch, null);
+          KitbotMotor.kDualCIMPerSide, KitbotGearing.k5p95, KitbotWheelSize.kSixInch, null);
 
   private double leftAppliedVolts = 0.0;
   private double rightAppliedVolts = 0.0;
